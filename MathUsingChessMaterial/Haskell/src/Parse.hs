e8fbbf1f5431114bcb7001d3e36cc1059d51b766
Rickiarty <fatalframe0719@gmail.com>
2023-12-19 13:20:37 +0800

    2023-12-19(Tue.)

--- MathUsingChessMaterial/Haskell/src/Parse.hs
+++ MathUsingChessMaterial/Haskell/src/Parse.hs
@@ -3,14 +3,17 @@ module Parse (parseStringToInt) where
 import Data.Char
 
 parseStringToInt :: [Char] -> Int 
-parseStringToInt [] = 0 
-parseStringToInt (c:cs) = if length cs /= 0 
-                        then (
-                                if '0' <= c && c <= '9' 
-                                then (ord c) - (ord '0') 
-                                else error "wrong format"
-                             ) * 10 + (parseStringToInt cs) 
-                        else 
-                            if '0' <= c && c <= '9' 
-                            then (ord c) - (ord '0') 
-                            else error "wrong format" 
+parseStringToInt str = parseStrToInt str (10 ^ ((length str) - 1))
+
+parseStrToInt :: [Char] -> Int -> Int 
+parseStrToInt [] _ = 0 -- empty string 
+parseStrToInt (c:cs) times = if length cs /= 0 -- not last char in string 
+                                then (
+                                        if '0' <= c && c <= '9' 
+                                        then (ord c) - (ord '0') 
+                                        else error "wrong format"
+                                      ) * times + (parseStrToInt cs (times `div` 10)) 
+                                else -- last char in string 
+                                    if '0' <= c && c <= '9' 
+                                    then (ord c) - (ord '0') 
+                                    else error "wrong format" 
